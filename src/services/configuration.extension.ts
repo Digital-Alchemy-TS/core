ccdef8d2585bf4d06bc9ee43e84137ba2fab25b6
Zoe Codez <github@zoe-codez.tools>
2024-10-28 18:33:06 -0500

    code fix

--- src/services/configuration.extension.ts
+++ src/services/configuration.extension.ts
@@ -42,7 +42,7 @@ export function Configuration({
   const configDefinitions: KnownConfigs = new Map();
   const configuration: PartialConfiguration = {};
   const loaded = new Set<string>();
-  const loaders = new Map<DataTypes, ConfigLoader>([["file", configLoaderFile]]);
+  const loaders = new Map<DataTypes, ConfigLoader>([]);
 
   // #MARK:
   const proxyData = {} as Record<string, object>;
@@ -138,7 +138,7 @@ export function Configuration({
       ["env", "argv"],
     );
     mergeConfig(
-      await ConfigLoaderEnvironment({
+      await configLoaderFile({
         application,
         configs: configDefinitions,
         internal,
